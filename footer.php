7147e58dc700b863f7b3a1de4815573d28b8bf71
Wendy May Kwok <wmkwok95@gmail.com>
2017-04-09 19:34:47 -0400

    changed look of tables

---
+++ footer.php
@@ -0,0 +1,16 @@
+
+<!-- links to contact and copyright information -->
+       <footer class="footer">
+
+          <div class="bg-faded">
+          <ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link" href="#">Contact Us</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Copyright</a>
+  </li>
+</ul>
+
+		   </div>
+        </footer>
